efcad94dce392b0b9294fe7ce69dd6b552060781
= <=>
2015-10-28 06:13:53 +0000

    Improved AppleConfig.cs

--- Apple/Application/Base/Config/AppleConfig.cs
+++ Apple/Application/Base/Config/AppleConfig.cs
@@ -8,22 +8,44 @@
 
     internal class AppleConfig
     {
-        private readonly Dictionary<string, string> _configValues = null;
-        private readonly FileInfo _configFile = null;
-        private readonly ILog _log = null;
+        /// <summary>
+        /// Holds a list of configuration keys and values.
+        /// </summary>
+        private readonly Dictionary<string, string> _configItems;
+
+        /// <summary>
+        /// Holds file information about the configuration file.
+        /// </summary>
+        private readonly FileInfo _configFile;
+
+        /// <summary>
+        /// Log4net logger.
+        /// </summary>
+        private readonly ILog _log;
+
+        /// <summary>
+        /// Checks if the class has been initialized already.
+        /// </summary>
         private readonly bool _initialized;
 
+        /// <summary>
+        /// AppleConfig constructor.
+        /// </summary>
+        /// <param name="filePath"></param>
         public AppleConfig(string filePath)
         {
             if (this._initialized)
                 return;
 
-            this._configValues = new Dictionary<string, string>();
+            this._configItems = new Dictionary<string, string>();
             this._configFile = new FileInfo(filePath);
             this._log = LogManager.GetLogger(typeof(AppleConfig));
             this._initialized = this.Initialize();
         }
 
+        /// <summary>
+        /// Initializes AppleConfig
+        /// </summary>
         private bool Initialize()
         {
             try
@@ -35,32 +57,55 @@
                     const int keyIndex = 0;
                     const int valueIndex = 1;
 
-                    this._configValues[splittedLine[keyIndex]] = splittedLine[valueIndex];
+                    this._configItems[splittedLine[keyIndex]] = splittedLine[valueIndex];
                 }
+
+                return true;
             }
             catch (Exception exception)
             {
                 _log.Error(exception); // assuming overload that takes an exception.
-                throw new ConfigurationUnavailableException(exception);
+                return false;
             }
         }
 
+        /// <summary>
+        /// Checks if a string is a valid config item.
+        /// </summary>
+        /// <param name="Line">String to check</param>
+        /// <returns></returns>
         private bool IsConfigurationLine(string Line)
         {
             return (!string.IsNullOrWhiteSpace(Line) && !Line.StartsWith("#") && Line.Contains("="));
         }
 
+        /// <summary>
+        /// Returns a config item by its key in the dictionary.
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <returns></returns>
         public string GetConfigElement(string Key)
         {
             if (!this.IsInitialized)
                 return null;
 
-            return this._configValues[Key];
+            return this._configItems[Key];
         }
 
+        /// <summary>
+        /// Checksa if the class has been initialized.
+        /// </summary>
         public bool IsInitialized
         {
             get { return this._initialized; }
+        }
+
+        /// <summary>
+        /// Refreshes config items from the config file.
+        /// </summary>
+        public void RefreshConfig()
+        {
+            this.Initialize();
         }
     }
 }
